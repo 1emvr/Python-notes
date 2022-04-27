6845ea961e4583637927df6d843a1313e906fa38
1emvr <103511211+1emvr@users.noreply.github.com>
2022-04-27 13:53:39 -0400

    Update compare.py

--- compare.py
+++ compare.py
@@ -21,7 +21,7 @@ compare()
 
 This enumerates too much. The program will run through all 3 options (or more, if you give it more), even if it's found the answer to the question. It will continue all the way till the end. We can make this more concise, so that when it's found a "correct" option, it automatically stops checking through the rest...
 
-Imagine if you had 30 of these if statements and the program had to run through all 30 before giving you a response, even if only 1 answer is true.
+Imagine if you had 30 of these if statements and the program had to run through all 30 before giving you a response, even if you've found the only answer.
 
 """
 
