c8593f1d4f54bb310a183dcad379c9608edf310f
1emvr <103511211+1emvr@users.noreply.github.com>
2022-04-27 13:58:42 -0400

    Update compare.py

--- compare.py
+++ compare.py
@@ -42,5 +42,5 @@ def compare():
 
 compare()
 
-### Rem3mbr, you can append "or" statements to be more concise, if the situation permits
+### Rem3mbr, you can append "or" statements to be more concise, if the situation permits. Same with "and"
 
