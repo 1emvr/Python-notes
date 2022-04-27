e6a846f0c552cb2c1a2567cc609cc10adb4a5ac8
1emvr <103511211+1emvr@users.noreply.github.com>
2022-04-27 14:12:30 -0400

    Add files via upload

---
+++ score.py
@@ -0,0 +1,25 @@
+
+
+score = int(input("Score: "))
+
+if score >= 96 and score <= 100:
+    print("A+")
+
+elif score >= 90 and score <= 95:
+    print("A")
+
+elif score >= 86 and score <= 89:
+    print("B+")
+
+elif score >= 80 and score <= 85:
+    print("B")
+
+elif score >= 76 and score <= 79:
+    print("C+")
+
+elif score >= 70 and score <= 75:
+    print("C")
+
+else:
+    print("try harder")
+
